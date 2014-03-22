e9a9628c6cef235167f98015cce9a66ccf5eba78
breizhpanda <kieffer41@hotmail.fr>
2014-03-22 12:55:32 +0100

    Ajout script LevelData
    Stock les données de construction des niveaux

---
+++ sources/config/LevelData.js
@@ -0,0 +1,45 @@
+var levelData =
+
+[
+		{
+			name       : Level 1,
+			data       : 0,
+			blockTable : {},
+			
+		},
+		
+		{
+			name       : Level 2,
+			data       : 1,
+			blockTable : {
+							block : {
+										x      : 500,
+										y      : 400,
+										width  : 100,
+										height : 100,
+									}
+						 }
+			
+		},
+		
+		{
+			name       : Level 3,
+			data       : 2,
+			blockTable : {
+							block : {
+										x      : ,
+										y      : ,
+										width  : ,
+										height : ,
+									}
+							block : {
+										x      : ,
+										y      : ,
+										width  : ,
+										height : ,
+									}
+			
+			
+						 },
+			
+		}];
