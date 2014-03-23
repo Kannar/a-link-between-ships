b1764b461ece638063e09d42a9fed53585db191d
breizhpanda <kieffer41@hotmail.fr>
2014-03-23 02:24:47 +0100

    Add Particle generator
    Particle generator for game VFX

---
+++ sources/config/datas/particleData.js
@@ -0,0 +1,31 @@
+var particleData = 
+	[
+		{
+		   type        : 0,
+		   name        : "explosion ennemies",
+		   nbParticles : 70,
+		   particleSpeed : 5,
+           color       : {
+							R : 250,
+							G : 100,
+							B : 100,
+						  },
+           lifetime     : 30,
+		   size         : 5
+           
+				
+		},
+		
+		{
+			type         : 1,
+		   name          : "explosion player",
+		   nbParticles   : 40,
+		   particleSpeed : 5,
+           color         : {
+							R : 250,
+							G : 100,
+							B : 100,
+						   },
+           lifetime      : 180,
+		   size          : 10
+		}];
